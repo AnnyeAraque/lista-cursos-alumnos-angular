c0ba09bfae4aba836d649be578eff23fb6784ffe
AnnyeAraque <annye9@live.com>
2024-03-16 11:35:42 -0500

    Primer repositorio Angular

---
+++ src/app/components/tabla-alumnos/mock.ts
@@ -0,0 +1,21 @@
+import { Alumno } from "../../models/alumno-to"
+
+export const alumnos: Alumno[] = [
+    {
+        nombre: 'Ricardo',
+        apellido: 'Perez',
+        promedio: 8.5
+    },
+
+    {
+        nombre: 'Maria',
+        apellido: 'Ramirez',
+        promedio: 9.3
+    },
+
+    {
+        nombre: 'Esteban',
+        apellido: 'Suarez',
+        promedio: 7
+    },
+]
---
+++ src/app/components/tabla-cursos/mock.ts
@@ -0,0 +1,22 @@
+import { Curso } from "../../models/curso-to";
+
+export const cursos: Curso[] = [
+    {
+        materia: 'Javascript',
+        dificultad: 9,
+        requiere: 'HTML y CSS'
+    },
+
+    {
+        materia: 'Typescript',
+        dificultad: 8.5,
+        requiere: 'Javascript'
+    },
+
+    {
+        materia: 'Angular',
+        dificultad: 9,
+        requiere: 'HTMl, CSS, Javascript, Typescript'
+    },
+    
+]
